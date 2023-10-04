febe5c9767e9894d4337f67d78a46df25f474a04
Roman Tsisyk <roman@tsisyk.com>
2023-10-04 11:10:40 +0300

    [android] Tag JNI methods and enable R8 optimizations
    R8 is not enabled for Debug, please test Beta or Release:
    
    ```
    ./gradlew -Parm64 runFdroidBeta
    ```
    
    Results:
    
    ```
    48534296 fdroid-beta-before.apk
    45663208 fdroid-beta-after.apk
    ```
    
    This patch saves almost 3MB or ~6%.
    
    Signed-off-by: Roman Tsisyk <roman@tsisyk.com>

--- android/app/src/main/java/app/organicmaps/bookmarks/BookmarksListFragment.java
+++ android/app/src/main/java/app/organicmaps/bookmarks/BookmarksListFragment.java
@@ -778,12 +778,6 @@ public class BookmarksListFragment extends BaseMwmRecyclerFragment<ConcatAdapter
   }
 
   @Override
-  public void onBookmarksLoadingStarted()
-  {
-    // No op.
-  }
-
-  @Override
   public void onBookmarksLoadingFinished()
   {
     View view = getView();
@@ -794,12 +788,6 @@ public class BookmarksListFragment extends BaseMwmRecyclerFragment<ConcatAdapter
     onViewCreatedInternal(view);
     updateRecyclerVisibility();
     updateLoadingPlaceholder(view, false);
-  }
-
-  @Override
-  public void onBookmarksFileLoaded(boolean success)
-  {
-    // No op.
   }
 
   private void updateLoadingPlaceholder(@NonNull View root, boolean isShowLoadingPlaceholder)
